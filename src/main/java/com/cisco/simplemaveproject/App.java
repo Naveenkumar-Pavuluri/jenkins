8ac6952cc7781cc2b5dd97994e09d8d9075404bd
Your Name <naveenpavuluri0910@gmail.com>
2024-02-29 12:19:41 +0530

    dummy

---
+++ src/main/java/com/cisco/simplemaveproject/App.java
@@ -0,0 +1,14 @@
+package com.cisco.simplemaveproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("welcome to Jenkins");
+    }
+}
