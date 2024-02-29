2e412cf8a4f97bb0c82c02830723561e571c99f1
Naveenkumar-Pavuluri <naveenpavuluri0910@gmail.com>
2024-02-29 15:09:57 +0530

    second commit

--- src/main/java/com/cisco/simplemaveproject/App.java
+++ src/main/java/com/cisco/simplemaveproject/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	HelloWorld.print();
         System.out.println( "Hello World!" );
         System.out.println("welcome to Jenkins");
     }
---
+++ src/main/java/com/cisco/simplemaveproject/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.cisco.simplemaveproject;
+
+public class HelloWorld {
+
+	public static void print() {
+		System.out.println("Hello From CI");
+	}
+	
+}
